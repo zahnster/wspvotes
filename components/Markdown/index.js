61c8cd1366d8e2ab520fe9ffa21d00cbd637427e
Jade Pennig <jade@pennig.name>
2018-07-28 21:13:38 -0500

    Add Markdown rendering Component.

--- components/Markdown/index.js
+++ components/Markdown/index.js
@@ -1,8 +1,13 @@
+import './style.scss'
+
 const md = require('markdown-it')({
   html: true,
   breaks: true
 })
 
 export default props => (
-  <div dangerouslySetInnerHTML={{ __html: md.render(props.content) }} />
+  <div
+    className="content-block"
+    dangerouslySetInnerHTML={{ __html: md.render(props.content) }}
+  />
 )
